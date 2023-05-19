f1f6920993d11825cc240a434a4872da56728d89
Bhavani Shankar Patra <bhavanipatra@gmail.com>
2023-05-19 15:00:19 +0530

    Initial commit

---
+++ src/test/java/com/bsp/blogappspringboot/BlogappSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsp.blogappspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogappSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
